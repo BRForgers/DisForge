b376e172998bbb6fcaf102bc0e8ae0058a5a81f0
Jônatas Pereira <jonatasp14@outlook.com.br>
2019-11-28 15:08:21 -0300

    Ooops i forgot something

--- src/main/java/me/jonatsp/disforge/DisForge.java
+++ src/main/java/me/jonatsp/disforge/DisForge.java
@@ -18,7 +18,7 @@ import org.apache.logging.log4j.Logger;
 public class DisForge {
     public static final String MOD_ID = "disforge";
     public static final String MOD_NAME = "DisForge";
-    public static final String VERSION = "1.12.2-1.0.0.0";
+    public static final String VERSION = "1.12.2-1.0.1.0";
     public static long startedTime;
     @Mod.Instance(MOD_ID)
     public static DisForge INSTANCE;
